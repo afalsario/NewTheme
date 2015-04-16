ef12d02adf2d3334c54f10212a176815c8e7bd20
Ashley <a_falsario@yahoo.com>
2015-04-16 00:08:16 -0500

    trimming out extra code

--- header.php
+++ header.php
@@ -12,27 +12,31 @@
     <meta name="Revisit-After" content="7 Days">
     <meta name="robots" content="index,follow">
     <!-- Properly enqueued stylesheets and scripts will appear in this function -->
+    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <?php wp_head(); ?>
 </head>
 <body>
 
     <div class="header">
-        <div class="wrap_1280">
-            <div class="logo">
-                <!-- Notice the WordPress function that dynamically creates the path to your theme -->
-                <a href='/picapicaplaza/index.php' title="PicaPica Plaza"><img src="<?php echo get_template_directory_uri(); ?>/images/Pica-Pica-Logo.png" alt="PicaPica Plaza"></a>
-            </div>
-
-            <div class="social-icons-header">
-                <div class="social-wrap pull-right">
-                    <div class="social-icons"><a href="http://www.facebook.com" target="_black"><img src="<?php echo get_template_directory_uri(); ?>/images/small-facebook.png" alt="Facebook Icon"></div>
-                    <div class="social-icons"><a href="http://www.youtube.com" target="_black"><img src="<?php echo get_template_directory_uri(); ?>/images/small-youtube.png" alt="youtube Icon"></div>
-                    <div class="social-icons"><a href="http://www.twitter.com" target="_black"><img src="<?php echo get_template_directory_uri(); ?>/images/small-twitter.png" alt="twitter Icon"></div>
-                    <div class="social-icons"><a href="http://www.yelp.com" target="_black"><img src="<?php echo get_template_directory_uri(); ?>/images/small-yelp.png" alt="yelp Icon"></div>
-                    <div class="clearfix"></div>
-                </div>
-            </div>
-            <div class="clearfix"></div>
+        <div class="social-icons-header">
+            <div class="social-icons"><a href="http://www.facebook.com" target="_black"><img src="<?php echo get_template_directory_uri(); ?>/images/small-facebook.jpg" alt="Facebook Icon"></div>
+            <div class="social-icons"><a href="http://www.youtube.com" target="_black"><img src="<?php echo get_template_directory_uri(); ?>/images/small-youtube.jpg" alt="youtube Icon"></div>
+            <div class="social-icons"><a href="http://www.twitter.com" target="_black"><img src="<?php echo get_template_directory_uri(); ?>/images/small-twitter.jpg" alt="twitter Icon"></div>
+            <div class="social-icons"><a href="http://www.yelp.com" target="_black"><img src="<?php echo get_template_directory_uri(); ?>/images/small-yelp.jpg" alt="yelp Icon"></div>
+        </div>
+        
+        <div class="banner"></div>
+        <div class="logo">
+            <!-- Notice the WordPress function that dynamically creates the path to your theme -->
+            <a href='/picapicaplaza/' title="PicaPica Plaza"><img src="<?php echo get_template_directory_uri(); ?>/images/Pica-Pica-Logo.png" alt="PicaPica Plaza"></a>
         </div>
     </div>
 
