92ecee08024a43a970097a025f7c9c3e07f4b195
Ashley Falsario <a_falsario@yahoo.com>
2015-04-19 01:31:32 -0500

    working on the feature box

--- content.php
+++ content.php
@@ -7,21 +7,27 @@
 Space
 
 <?php if($post->post_content !== "") :?>
-	<div class="row">
-		<?php the_content();?>
-	</div>
 
-	<?php	if ( has_post_thumbnail() ) { ?>
 
 	<div id="event">
 		<div class="row">
-			<div id="event-photo-box">
-				<?php the_post_thumbnail(); } ?>
+			<?php if ( has_post_thumbnail() ) { ?>
+					
+			<div class="event-photo-box small-12 medium-4 large-4 columns">
+				<?php the_post_thumbnail(); ?>
 			</div>
+
+			<div class="small-12 medium-4 large-4 columns feature-content">
+				<?php the_content(); ?>
+			</div>
+
+			<div class="small-12 medium-4 large-4 columns"><a href="#" class="small button round feature-button">Round Button</a></div>
+
+			<?php } ?>
 		</div>
 	</div>
 
 	<div class="clearfix"></div>
-
+Space
 <?php endif; ?>
 
